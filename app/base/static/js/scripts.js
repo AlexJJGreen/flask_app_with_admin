e0e4a9c1306c8955f506aac6b5412d88a1ebc26a
AlexJJGreen <alexjjgreen@gmail.com>
2023-10-11 12:56:32 +0200

    header nav functionality and dynamics added

--- app/base/static/js/scripts.js
+++ app/base/static/js/scripts.js
@@ -1,29 +1,14 @@
-(function () {
-    let $curve = document.getElementById("curve");
-    let lastKnownScrollPos = 0;
-    let defaultCurveVal = 350;
-    let curveRate = 3;
-    let ticking = false
-    let curveVal;
+(
+    () => {
+        let menuIcon = document.getElementById("menuIcon");
+        let menu = document.getElementById("menu");
 
-    function scrollEvent(scrollPos) {
-        if (scrollPos >= 0 && scrollPos < defaultCurveVal) {
-            curveVal = defaultCurveVal - parseFloat(scrollPos / curveRate);
-            $curve.setAttribute("d", "M 800 300 Q 400 " + curveVal + " 0 300 L 0 0 L 800 0 L 300 Z");
-        }
-    }
+        menuIcon.addEventListener("click", () => {
+            if (menu.classList.contains("expanded")) {
+                menu.classList.remove("expanded");
+            } else {
+                menu.classList.add("expanded");
+            }
+        });
 
-    // set listener
-    window.addEventListener("scroll", (e) => {
-        lastKnownScrollPos = window.scrollY;
-
-        if (!ticking) {
-            window.requestAnimationFrame(() => {
-                scrollEvent(lastKnownScrollPos);
-                ticking = false;
-            });
-        }
-
-        ticking = true;
-    });
-})();
+    })();
