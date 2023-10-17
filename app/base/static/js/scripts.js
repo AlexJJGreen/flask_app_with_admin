9e9cebb45d40d1872d9343b52992d71eaca0c572
AlexJJGreen <alexjjgreen@gmail.com>
2023-10-17 13:42:10 +0200

    Main Article

--- app/base/static/js/scripts.js
+++ app/base/static/js/scripts.js
@@ -6,8 +6,10 @@
         menuIcon.addEventListener("click", () => {
             if (menu.classList.contains("expanded")) {
                 menu.classList.remove("expanded");
+                menuIcon.firstChild.classList = "fa-solid fa-bars";
             } else {
                 menu.classList.add("expanded");
+                menuIcon.firstChild.classList = "fa-solid fa-minus";
             }
         });
 
