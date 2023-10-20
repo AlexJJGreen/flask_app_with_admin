71aef061ea7e562afcb3083d8e4bcfaee063a6c4
AlexJJGreen <alexjjgreen@gmail.com>
2023-10-20 14:57:36 +0200

    bug fix, malformed static url paths

--- app/editor/__init__.py
+++ app/editor/__init__.py
@@ -5,7 +5,7 @@ bp = Blueprint(
     __name__,
     template_folder="templates",
     static_folder="static",
-    static_url_path="/static",
+    static_url_path="/editor",
 )
 
 from . import routes
