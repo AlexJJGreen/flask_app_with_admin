d0e46def555d94ee091c0335a6df22839bf17c2c
AlexJJGreen <alexjjgreen@gmail.com>
2023-10-20 14:47:18 +0200

    database migration - post table

--- app/models.py
+++ app/models.py
@@ -17,3 +17,11 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140), index=True)
+    snippet = db.Column(db.String(280), index=True)
+    thumbnail_url = db.Column(db.String(280), index=True)
+    content = db.Column(db.Text)
